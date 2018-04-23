1eae19d9d3b7769d5876779d1045d80393169826
Jack Dauphars <jdauphar@kent.edu>
2018-04-23 19:46:38 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,17 +17,6 @@ pip3 install pyvirtualdisplay==0.2.1
 #selenium to let us use webdriver
 pip3 install selenium==3.11.0
 
-echo 'installing file transfer packages'
-#regular packages
-#cifs to mount the samba server
-sudo apt install cifs-utils=2:6.7-1
-
-#rsync to copy to samba server
-sudo apt install rsync=3.1.2-1+deb9u1
-
-#crontab to set hourly task to run the script
-sudo apt install cron=3.0pl1-128+deb9u1
-
 #geckodriver for headless firefox
 wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
 sudo tar -xvzf geckodriver-v0.18.0-linux64.tar.gz geckodriver
